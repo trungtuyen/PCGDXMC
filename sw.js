ac36ea9e0c2872200ba2e6105e6e81dc1e583105
Nguyễn Trung Tuyền <tuyenthcskimhy@gmail.com>
2026-08-29 21:59:52 +0700

    Nâng cache cho danh sách mù chữ mức độ 1 và 2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pcgdxmc-v0.8.0';
-const CORE=['./','./index.html','./styles.css','./core-v02.js','./reports-v02.js','./groups-v03.js','./viewer-v04.js','./app.js','./manifest.webmanifest'];
+const CACHE='pcgdxmc-v0.9.0';
+const CORE=['./','./index.html','./styles.css','./core-v02.js','./reports-v02.js','./groups-v03.js','./viewer-v04.js','./app.js','./xmc-lists-v09.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
