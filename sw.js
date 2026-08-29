1c37793657614d0422641c26f1e400beebcd04d6
Nguyễn Trung Tuyền <tuyenthcskimhy@gmail.com>
2026-08-29 21:27:53 +0700

    Nâng cache Beta 0.5 cho nhập nhiều phiếu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pcgdxmc-v0.4.0';
+const CACHE='pcgdxmc-v0.5.0';
 const CORE=['./','./index.html','./styles.css','./core-v02.js','./reports-v02.js','./groups-v03.js','./viewer-v04.js','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
