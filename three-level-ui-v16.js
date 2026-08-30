5275c8e9c5f14077ad96090408d6fbea72630614
Nguyễn Trung Tuyền <tuyenthcskimhy@gmail.com>
2026-08-30 19:57:37 +0700

    Nạp biểu nhập trực tiếp Tiểu học trên web

--- three-level-ui-v16.js
+++ three-level-ui-v16.js
@@ -1,12 +1,17 @@
 (function(){
   'use strict';
   const ui=document.createElement('script');
-  ui.src='three-level-ui-v17.js?v=20260830-local-v19';
+  ui.src='three-level-ui-v17.js?v=20260830-local-v20';
   ui.async=false;
   document.head.appendChild(ui);
 
   const local=document.createElement('script');
-  local.src='local-mode-v18.js?v=20260830-local-v19';
+  local.src='local-mode-v18.js?v=20260830-local-v20';
   local.async=false;
   document.head.appendChild(local);
+
+  const thManual=document.createElement('script');
+  thManual.src='th-manual-input-v20.js?v=20260830-local-v20';
+  thManual.async=false;
+  document.head.appendChild(thManual);
 })();
